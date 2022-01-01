b1d4ef26df0f776817b23340b10e51764a2401ad
Pavan <5058918-gitme3@users.noreply.gitlab.com>
2022-01-01 22:11:10 +0530

    new char

--- sample.sh
+++ sample.sh
@@ -1,2 +1,9 @@
 #!/usr/bin/bash
-echo -e "\e[COLmMESSAGE\e[0m"
+echo "Playing with colors"
+echo "======================================"
+echo -e "\e[31mHello In RED\e[0m"
+echo -e "\e[32mHello In GREEN\e[0m"
+echo -e "\e[33mHello In YELLOW\e[0m"
+echo -e "\e[34mHello In BLUEe[0m"
+echo -e "\e[35mHello In MAGENTA\e[0m"
+echo -e "\e[36mHello In CYAN\e[0m"
