d5e2e5f45052bf9f225595b5c50861a29221b643
Pavan <5058918-gitme3@users.noreply.gitlab.com>
2022-01-08 07:27:17 +0530

    new

--- color-play.sh
+++ color-play.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/bash
 echo "Playing with colors"
-echo "===================="
+echo "====================="
 echo -e "\e[31mHello In RED\e[0m"
 echo -e "\e[32mHello In GREEN\e[0m"
 echo -e "\e[33mHello In YELLOW\e[0m"
 echo -e "\e[34mHello In BLUEe[0m"
 echo -e "\e[35mHello In MAGENTA\e[0m"
 echo -e "\e[36mHello In CYAN\e[0m"
-echo "===================="
+echo "====================="
 
