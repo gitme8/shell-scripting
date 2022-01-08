e35a131ba2ef36587ed96e2dc9bba49345555d5b
Pavan <5058918-gitme3@users.noreply.gitlab.com>
2022-01-08 07:42:28 +0530

    hello

--- age.sh
+++ age.sh
@@ -2,9 +2,9 @@
 
 read -p 'Please enter your age: ' age
 
-if [ $"{age}" -gt 18 ]; then
+if [ "${age}" -gt 18 ]; then
     echo you are major
-elif [ $"{age}" -gt 60 ]; then
+elif [ "${age}" -gt 60 ]; then
     echo you are senior citizen
 else
   echo you are minor
